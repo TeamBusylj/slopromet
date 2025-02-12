1019f48bc193b717d0c63364babdfd7d7f5d8a4a
Tim Bizilj <tim.bizilj@gmail.com>
2025-02-12 22:18:31 +0100

    d

--- helper.js
+++ helper.js
@@ -398,7 +398,7 @@ async function generateRouteVector(data, trip_id, lno, lid) {
         );
         setTimeout(() => {
           document.querySelector(".bottomSheet").style.transform =
-            "translate3d(-50%,98dvh, 0)";
+            "translate3d(-50%,2dvh, 0)";
           sheetHeight = 98;
           document.querySelector(".sheetContents").scrollTop = 0;
         }, 50);
