3b65e02ebcd4bae975a7b8005b69e3cbc319bf4f
Tim Bizilj <tim.bizilj@gmail.com>
2025-07-16 13:55:31 +0200

    feat: add oppositeStation function to handle station transitions in arrivals panel

--- public/assets/js/skupna.js
+++ public/assets/js/skupna.js
@@ -4226,3 +4226,34 @@ window.onpopstate = function (event) {
     if (iks.getBoundingClientRect().left > 0) iks.click();
   });
 };
+async function oppositeStation(id) {
+  if (agency !== "lpp") id = id.split(":")[1];
+  let arS = document.getElementById("arrivals-panel");
+  arS.style.transform = "translateX(0px) translateY(-20px)";
+  arS.style.opacity = "0";
+  clearInterval(interval);
+  setTimeout(async () => {
+    document.querySelector(".arrivalsHolder").remove();
+    document.querySelector(".infoBar").remove();
+    let i = document.querySelector(".timeTScroll");
+    i = clearElementContent(document.querySelector(".timeTScroll"));
+    if (id % 2 === 0) {
+      await stationClick(
+        stationList.findIndex((obj) => obj.ref_id === String(parseInt(id) - 1)),
+        1,
+        1
+      );
+    } else {
+      await stationClick(
+        stationList.findIndex((obj) => obj.ref_id === String(parseInt(id) + 1)),
+        1,
+        1
+      );
+    }
+    arS = document.getElementById("arrivals-panel");
+    setTimeout(() => {
+      arS.style.transform = "translateX(0px) translateY(0px)";
+      arS.style.opacity = "1";
+    }, 1);
+  }, 100);
+}
